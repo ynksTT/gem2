af3df746d228c8e553de8b5f3ed5bf3766b7d39a
ynksTT <159096223+ynksTT@users.noreply.github.com>
2024-08-01 18:29:01 +0900

    PHP
    トップページ（index.html）をindex.php/home.phpに変更し、index.phpのimgや各aタグのURLをワードプレス用に変更

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+    <title>名古屋市八事: ラグ・ペルシャ絨毯専門店 G.E.M</title>
+    <meta charset="utf-8">
+    <meta name="description" content="名古屋市昭和区八事にあるインテリアショップG.E.M（ギャラリーエクセレンスモダン）です。オールドトライバル（ヴィンテージラグ）、モロッコラグ、ペルシャ絨毯など">
+    
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="icon" href="img/favic　GEMのアイコン写真">
+    <link rel="stylesheet" href="style.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+  </head>
+
+
+  <body>
+    <section id="about_1">
+      <div class="about_1_left">
+        <h1 class="about_1_title">G.E.M</h1>
+        <nav id="about_1_nav">
+          <div class="about_1_nav_a">
+            <a href="about_3.html" id="about_site">About</a>
+          </div>
+          <div class="about_1_nav_a">
+            <a href="product.html" id="product_site">Product</a>
+          </div>
+          <div class="about_1_nav_a">
+            <a href="review.html" id="review_site">Review</a>
+          </div>
+          <div class="about_1_nav_a">
+            <a href="access.html" id="access_site">Access</a>
+          </div>
+          <div class="about_1_nav_a">
+            <a href="#">Online Store</a>
+          </div>
+          <div class="about_1_nav_a">
+            <a href="#" id="news_site">News</a>
+          </div>
+          <a href="">
+            <img src="images/icon/icon_062470_256.png" alt="instagram">
+          </a>
+        </nav>
+      </div>
+      <!-- <div class="about_1_right">
+        <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+        <h1>IN YOUR SEASON PLAGE</h1>
+        <p class="about_1_center_p2">G.E.Mは、流行りとは違う[旬]を見つけるインテリアショップ</p>
+        <a href="">
+          <p class="about_1_center_p3">READ MORE</p>
+        </a>
+      </div> -->
+      <div id="about1_main">
+        <div class="about_site">
+          <div class="about_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>IN YOUR SEASON PLAGE</h2>
+            <p class="about_1_center_p2_2">G.E.Mは、流行りとは違う「旬」を見つけるインテリアショップ</p>
+            <nav class="about_1_main_nav">
+              <a href="about_3.html" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>
+
+        <div class="product_site">
+          <div class="product_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>G.E.M SPECIAL COLLECTION</h2>
+            <p class="about_1_center_p2">国内外からセレクトされたこだわりの逸品</p>
+            <nav class="about_1_main_nav">
+              <a href="product.html" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>  
+
+        <div class="review_site">
+          <div class="review_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>USER’S VOICE</h2>
+            <p class="about_1_center_p2_2">G.E.Mでご注文いただいたお客様の暮らし声をレビュー</p>
+            <nav class="about_1_main_nav">
+              <a href="review.html" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>  
+
+        <div class="access_site">
+          <div class="access_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>ACCESS GUIDE</h2>
+            <p class="about_1_center_p2_2">名古屋市八事にある、インテリアショップG.E.M</p>
+            <nav class="about_1_main_nav">
+              <a href="access.html" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>  
+
+        <div class="access_site">
+          <div class="access_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>ACCESS GUIDE</h2>
+            <p class="about_1_center_p2_2">名古屋市八事にある、インテリアショップG.E.M</p>
+            <nav class="about_1_main_nav">
+              <a href="" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>  
+
+        <div class="news_site">
+          <div class="news_site2">
+            <p class="about_1_center_p1">CREATE A BEAUTIFUL SPACE</p>
+            <h2>NEWS</h2>
+            <p class="about_1_center_p2_2">名古屋市八事にある、インテリアショップG.E.M</p>
+            <nav class="about_1_main_nav">
+              <a href="" class="about_1_center_a">READ MORE</a>
+            </nav>
+          </div>
+        </div>  
+   
+    </div>
+
+  </section>
+
+       
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script src="script.js"></script>
+  </body>
+ 
+</html>
